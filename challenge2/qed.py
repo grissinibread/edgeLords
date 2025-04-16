1b4eff4f38fa444a2d75f03db407999337804fd8
adria <varga418@csusm.edu>
2025-04-15 22:32:47 -0700

    file updated to be more efficient with 6n +/- 1 and stores numbers in txt file

--- challenge2/qed.py
+++ challenge2/qed.py
@@ -23,8 +23,9 @@ def isPrime(p):
 print("Odd numbers that cannot be calculated with given formula:")
 # only evaluate odd nums
 # k = p + 2*n*n
+count = 0
 with open("output.txt", "w") as f: #refrence output.txt for numbers
-    for k in range(5775, 100000, 2):
+    for k in range(5775, sys.maxsize, 2):
         valid = False
 
         for n in range(0, math.floor(math.sqrt(k)), 1):
@@ -34,7 +35,11 @@ with open("output.txt", "w") as f: #refrence output.txt for numbers
                 break
 
         if not valid:
+                count += 1
                 f.write(str(k)+"\n") #stores in file
                 print(str(k))
+
+        if count == 2: #only 2 possible solutions
+            break
 
 print("Finished!")
